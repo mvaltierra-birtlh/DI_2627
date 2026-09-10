1cbfd1ca7db6fa0a19da6c016077185fec1ca44a
cvaltierra <mvaltierra@ikaslea.birt.eus>
2026-09-11 00:27:23 +0200

    Creating first Ionic project

---
+++ Unit00/DI00_FirstAPP/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'DI00_FirstAPP',
+  webDir: 'www'
+};
+
+export default config;
